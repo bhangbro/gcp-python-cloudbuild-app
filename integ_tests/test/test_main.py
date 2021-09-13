55d07e88e4a9dde32e8079bbf17bae1093d9d010
bhangbro <brashidnia@gmail.com>
2021-09-13 06:59:14 -0700

    Fix endpoint

--- integ_tests/test/test_main.py
+++ integ_tests/test/test_main.py
@@ -4,7 +4,7 @@ import os
 import ssl
 import unittest
 
-endpoint = os.environ.get("API_ENDPOINT", "python-flask-service-shnjd7oeia-uc.a.run.app")
+endpoint = os.environ.get("API_ENDPOINT", "python-flask-service-shnjd7oeia-ue.a.run.app")
 
 context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
 
