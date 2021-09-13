2a3ed3309fc6211f77dfe79f9086d9e3e9103949
bhangbro <brashidnia@gmail.com>
2021-09-13 06:23:34 -0700

    Add integration tests

---
+++ integ_tests/test/test_main.py
@@ -0,0 +1,27 @@
+import http.client
+import json
+import os
+import ssl
+import unittest
+
+endpoint = os.environ.get("API_ENDPOINT")
+
+context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+
+class MainIntegrationTests(unittest.TestCase):
+
+    def test_hello_world(self):
+        connection = http.client.HTTPSConnection(endpoint, context=context)
+        headers = {'Content-type': 'application/json'}
+        connection.request('GET', '/', headers=headers)
+
+        response = connection.getresponse()
+        print(response.status)
+        print(response.reason)
+        r = response.read().decode()
+        print(r)
+        connection.close()
+        self.assertEqual('Hello World!', r)
+
+if __name__ == '__main__':
+    unittest.main()
